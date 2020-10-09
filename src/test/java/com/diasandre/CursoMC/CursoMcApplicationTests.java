6d3467d60b425580e28c9a76a4a04eca7f4a0993
andreepdias <andre.dias@msn.com>
2020-10-09 18:39:56 -0300

    Projeto criado

---
+++ src/test/java/com/diasandre/CursoMC/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diasandre.CursoMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
