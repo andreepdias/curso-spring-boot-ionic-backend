b394e5a0cdded1dea5cc2dab838f8ab3674aeea8
andreepdias <andre.dias@msn.com>
2020-10-10 15:23:57 -0300

    Estado e Cidade

--- src/main/java/com/diasandre/CursoMC/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/diasandre/CursoMC/resources/exceptions/ResourceExceptionHandler.java
@@ -13,10 +13,10 @@ import com.diasandre.CursoMC.services.exceptions.ObjectNotFoundException;
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
-		
-		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage() , System.currentTimeMillis());
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
+
 }
