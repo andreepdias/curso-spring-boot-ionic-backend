84e682003cd0134ddddd97f7e04b4ffac1bb188f
andreepdias <andre.dias@msn.com>
2020-10-10 15:10:35 -0300

    .

--- src/main/java/com/diasandre/CursoMC/domain/Produto.java
+++ src/main/java/com/diasandre/CursoMC/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -26,8 +28,8 @@ public class Produto implements Serializable{
 	@JoinTable(name = "Produto_Categoria",
 		joinColumns = @JoinColumn(name = "produto_id"),
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
-	)
-	
+	)	
+	@JsonBackReference
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	public Produto() {		
