fd32a29df903b3decfa114284ef21255f83b3632
andreepdias <andre.dias@msn.com>
2020-10-11 18:35:43 -0300

    PUT, DELTE, GET para Cliente

--- src/main/java/udemySpringBoot/domain/Cliente.java
+++ src/main/java/udemySpringBoot/domain/Cliente.java
@@ -49,7 +49,7 @@ public class Cliente  implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo =  (tipo == null) ? null : tipo.getCod();
 	}
 
 	public Integer getId() {
@@ -85,7 +85,7 @@ public class Cliente  implements Serializable {
 	}
 
 	public String getTipo() {
-		return TipoCliente.toEnum(tipo).getDescricao();
+		return (tipo == null) ? null : TipoCliente.toEnum(tipo).getDescricao();
 	}
 
 	public void setTipo(TipoCliente tipo) {
