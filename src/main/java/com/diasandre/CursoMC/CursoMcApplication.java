268a4ac22834a46a87e558001693c33c80ad23ec
andreepdias <andre.dias@msn.com>
2020-10-12 16:28:12 -0300

    Configuração e envio do projeto para Produção

---
+++ src/main/java/com/diasandre/CursoMC/CursoMcApplication.java
@@ -0,0 +1,20 @@
+package com.diasandre.CursoMC;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		
+	}
+
+}
---
+++ src/main/java/com/diasandre/CursoMC/services/PedidoService.java
@@ -0,0 +1,64 @@
+package com.diasandre.CursoMC.services;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.diasandre.CursoMC.domain.ItemPedido;
+import com.diasandre.CursoMC.domain.PagamentoComBoleto;
+import com.diasandre.CursoMC.domain.Pedido;
+import com.diasandre.CursoMC.domain.enums.EstadoPagamento;
+import com.diasandre.CursoMC.repositories.ItemPedidoRepository;
+import com.diasandre.CursoMC.repositories.PagamentoRepository;
+import com.diasandre.CursoMC.repositories.PedidoRepository;
+import com.diasandre.CursoMC.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository; 
+	
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+				+ ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
+}
