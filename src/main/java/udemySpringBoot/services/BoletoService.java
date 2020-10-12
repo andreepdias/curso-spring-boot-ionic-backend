edce2d5feea2ce010662281c628f42e018a01a33
andreepdias <andre.dias@msn.com>
2020-10-12 11:30:07 -0300

    Inserir Pedido

---
+++ src/main/java/udemySpringBoot/services/BoletoService.java
@@ -0,0 +1,19 @@
+package udemySpringBoot.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import udemySpringBoot.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar ca1 = Calendar.getInstance();
+		ca1.setTime(instanteDoPedido);
+		ca1.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(ca1.getTime());
+	}
+}
