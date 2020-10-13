2f5ce4135d10024ed01e8e8d8207f25f081f369a
andreepdias <andre.dias@msn.com>
2020-10-13 17:57:18 -0300

    Implementando Autorização

--- src/main/java/udemy/config/SecurityConfig.java
+++ src/main/java/udemy/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import udemy.security.JWTAuthenticationFilter;
+import udemy.security.JWTAuthorizationFilter;
 import udemy.security.JWTUtil;
 
 import java.util.Arrays;
@@ -55,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
