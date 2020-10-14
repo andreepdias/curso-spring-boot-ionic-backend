79d5250468c0132e1db5a36cde59006d30fc17e3
andreepdias <andre.dias@msn.com>
2020-10-14 13:54:29 -0300

    Enviando imagem por endpoint

--- src/main/java/udemy/Application.java
+++ src/main/java/udemy/Application.java
@@ -7,14 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		
-	}
+    @Override
+    public void run(String... args) throws Exception {
+    }
 
 }
